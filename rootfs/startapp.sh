3a6004097709b55efc1e1bff850b48ae56281abd
Chris Sandvik <chris.sandvik@gmail.com>
2021-11-18 23:19:45 -0500

    Attempt to start with window maximized

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-mono /defaults/SubtitleEdit.exe
+
+# # Install requested packages.
+# if [ "${INSTALL_EXTRA_PKGS:-UNSET}" != "UNSET" ]; then
+#   log "installing requested package(s)..."
+#   for PKG in $INSTALL_EXTRA_PKGS; do
+#     if cat /etc/apk/world | grep -wq "$PKG"; then
+#       log "package '$PKG' already installed"
+#     else
+#       log "installing '$PKG'..."
+#       run add-pkg "$PKG" \| log 2>&1
+#     fi
+#   done
+# fi
+
+# # Make sure mandatory directories exist.
+# mkdir -p /config/logs
+
+mono /defaults/SubtitleEdit.exe --window max
